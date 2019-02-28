4670e6a0ecffd3639492512e55264cac0008d01d
Justus Rossmeier <veecue@users.noreply.github.com>
2019-02-28 09:57:05 +0100

    enhancement: implement proper caching and serve dependencies.js (#18)

--- middleware/nocache.go
+++
@@ -1,9 +0,0 @@
-package middleware
-
-import "github.com/gin-gonic/gin"
-
-// NoCache sets Cache-Control to "no-cache"
-func NoCache(c *gin.Context) {
-	c.Next()
-	c.Header("Cache-Control", "no-cache")
-}
